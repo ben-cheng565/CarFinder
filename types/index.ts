8c1135b2e57deca087eb52ed639afcb2d47ff38c
Ben Cheng <ben.c@spritely.co.nz>
2023-07-07 13:00:15 +1200

    feat: Create types and uitls

--- types/index.ts
+++ types/index.ts
@@ -4,5 +4,51 @@ export interface CustomButtonProps {
   title: string;
   btnType?: "button" | "submit";
   containerStyles?: string;
+  textStyles?: string;
+  rightIcon?: string;
+  isDiasbled?: boolean;
   handleClick?: MouseEventHandler<HTMLButtonElement>;
+}
+
+export interface SearchManufacturerProps {
+  manufacturer: string;
+  setManufacturer: (manufacturer: string) => void;
+}
+
+export interface CarProps {
+  city_mpg: number;
+  class: string;
+  year: number;
+  combination_mpg: number;
+  cylinders: number;
+  displacement: number;
+  drive: string;
+  fuel_type: string;
+  highway_mpg: number;
+  make: string;
+  model: string;
+  transmission: string;
+}
+
+export interface FilterProps {
+  manufacturer: string;
+  model: string;
+  year: number;
+  fuel: string;
+  limit: number;
+}
+
+export interface OptionProps {
+  title: string;
+  value: string;
+}
+
+export interface CustomFilterProps {
+  title: string;
+  options: OptionProps[];
+}
+
+export interface ShowMoreProps {
+  pageNumber: number;
+  isNext: number;
 }
---
+++ utils/index.ts
@@ -0,0 +1,42 @@
+import axios from "axios";
+import { FilterProps } from "@/types";
+
+export async function fetchCars(filters: FilterProps) {
+  const { manufacturer, model, year, fuel, limit } = filters;
+
+  const headers = {
+    "X-RapidAPI-Key": "02f8d474admshb2cbac47d6ec706p1e569fjsnde7fe0e96dc3",
+    "X-RapidAPI-Host": "cars-by-api-ninjas.p.rapidapi.com",
+  };
+
+  const url = `https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?make=${manufacturer}&model=${model}&year=${year}&fuel_type=${fuel}&limit=${limit}}`;
+  const response = await axios.get(url, { headers });
+  const result = await response.data;
+
+  return result;
+}
+
+export const calculateCarRent = (city_mpg: number, year: number) => {
+  const basePricePerDay = 50; // Base rental price per day in dollars
+  const mileageFactor = 0.1; // Additional rate per mile driven
+  const ageFactor = 0.05; // Additional rate per year of vehicle age
+
+  // Calculate additional rate based on mileage and age
+  const mileageRate = city_mpg * mileageFactor;
+  const ageRate = (new Date().getFullYear() - year) * ageFactor;
+
+  // Calculate total rental rate per day
+  const rentalRatePerDay = basePricePerDay + mileageRate + ageRate;
+
+  return rentalRatePerDay.toFixed(0);
+};
+
+export const updateSearchParams = (type: string, value: string) => {
+  const params = new URLSearchParams(window.location.search);
+
+  params.set(type, value);
+
+  const queryString = `${window.location.pathname}?${params.toString()}`;
+
+  return queryString;
+};
